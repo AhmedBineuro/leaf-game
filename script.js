ac64070bbe6b84b9ab372469ca12df6171897ec1
Ahmed Abdalla <dynamic.bineuro@gmail.com>
2023-10-21 14:56:54 -0700

    Took the click handling function outside

--- script.js
+++ script.js
@@ -1,19 +1,26 @@
 //Initialize grid space as a table and add the onclick event listener
 
 const gameboardContainer = document.getElementById('gameboardContainer')
+generateGrid(15);
 
-for (let i = 0; i < 15; i++){
-    let row=gameboardContainer.insertRow();
-    for(let j=0;j<15;j++){
-        let gamePiece = row.insertCell();
-        gamePiece.setAttribute("xPos",j);
-        gamePiece.setAttribute("yPos",i);
-        gamePiece.classList.add("gamePiece");
-        gamePiece.onclick= function(){
-            let coordinates={"x":-1,"y":-1}
-            coordinates.x=this.cellIndex;
-            coordinates.y=this.parentNode.rowIndex;
-            console.log(coordinates);
-        };
+function generateGrid(gridSize){
+    for (let i = 0; i < gridSize; i++){
+        let row=gameboardContainer.insertRow();
+        for(let j=0;j<gridSize;j++){
+            let gamePiece = row.insertCell();
+            gamePiece.setAttribute("xPos",j);
+            gamePiece.setAttribute("yPos",i);
+            gamePiece.classList.add("gamePiece");
+            gamePiece.onclick= handClick
+        }
     }
+}
+
+
+//Function to handle cell based clicks
+function handClick(){
+    let coordinates={"x":-1,"y":-1}
+                coordinates.x=this.cellIndex;
+                coordinates.y=this.parentNode.rowIndex;
+                console.log(coordinates);
 }
